34c2e88d9701f054b3d91e4e3bef2664e7c7d306
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2024-12-30 18:32:41 +0000

    Atualizar

--- services/boardgames.service.js
+++ services/boardgames.service.js
@@ -56,6 +56,6 @@ const boardgamesService = {
         return firebase.firestore()
             .collection("boardgames")
             .doc(boardgameid)
-            .arrayUnion(players)
+            .arrayUnion(player)
     }
 };
