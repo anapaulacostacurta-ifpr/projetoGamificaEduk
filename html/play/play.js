6a85af4dd43ad7a02bcdb488a88b255a7de92b1a
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2024-12-30 01:59:44 +0000

    Atualizar

--- html/play/play.js
+++ html/play/play.js
@@ -8,7 +8,7 @@ document.getElementById("play-form").addEventListener("submit", function(event) 
   const player = document.getElementById("userUid").value.split(",").map(players => players.trim());
   const players = new Array(player);
   
-  boardgamesService.findByUid(boardgameid).then( (boardgames) => {
+  boardgamesService.getBoardGameByID(boardgameid).then((boardgames) => {
     console.log(boardgames);
     const round_date = boardgames.round_date;
     const level = boardgames.level;
--- services/boardgames.service.js
+++ services/boardgames.service.js
@@ -8,6 +8,23 @@ const boardgamesService = {
                 return doc.data();
             });
     },
+    getBoardGameByID: async (boardgameid) => {
+        try {
+            const querySnapshot = await firebase.firestore().collection("boardgames")
+            .where('boardgamesid','==',boardgameid)
+            .get();
+
+            if(querySnapshot.empty){
+                throw new Error("Nenhuma pergunta encontrada para o nível "+ level+ " .");
+            }
+            const boardgame = querySnapshot.docs.map(doc=>doc.data());
+            return boardgame;
+        } catch (error) {
+                console.error("Erro ao carregar perguntas:", error);
+                alert("Falha ao carregar perguntas. Tente novamente mais tarde.");
+                return [];
+        }
+    },
     remove: boardgames => {
         return firebase.firestore()
             .collection("boardgames")
