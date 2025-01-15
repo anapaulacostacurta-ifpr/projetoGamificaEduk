15fc7083ff418477142ed42c99fcd13516d621ff
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2025-01-15 14:01:49 +0000

    Atualizar

--- auth-guard.js
+++ auth-guard.js
@@ -1,6 +1,6 @@
 firebase.auth().onAuthStateChanged( (user) => {
     if (!user) {
-        window.location.href = "/html/login/login.html";
+        window.location.href = "../../html/login/login.html";
     }
     const userUid = user.uid;
     document.getElementById("userUid").value = user.uid;
