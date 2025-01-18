5cbf4456df343aa3aa71740f0b37f1787d01777b
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2025-01-18 01:07:27 +0000

    Atualizar

--- html/play/play.js
+++ html/play/play.js
@@ -11,21 +11,22 @@ document.getElementById("play-form").addEventListener("submit", function(event) 
     boardgames.forEach(boardgame => {
       console.log(boardgames);
       const state  = boardgame.state;
-      const players_old = boardgame.players;
-      console.log(players_old);
+      let i = arrayLength(boardgame.players);
       if(state != "started"){
         alert('Tabuleiro Não disponível ainda. Fale com o professor!');
       }else{
-        if (players_old === undefined){
-          const players = {players:{0:{user_UID:user_UID,score_round:0}}};
-          boardgamesService.addPlayers(boardgame_id, players);
-        }else{
-          const i = players_old.length;
-          const players = {players:{i:{user_UID:user_UID,score_round:0}}};
-          boardgamesService.addPlayers(boardgame_id, players);
-        }
+        const players = {players:{i:{user_UID:user_UID,score_round:0}}};
+        boardgamesService.addPlayers(boardgame_id, players);
       }
       })
       document.getElementById("play-form").reset();
     });
   });
+
+  function arrayLength(arr) {
+    let count = 0;
+    for (const element of arr) {
+      count++;
+    }
+    return count; 
+ }
