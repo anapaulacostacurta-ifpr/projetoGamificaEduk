80d1bd00d33bbb17228f1005649b12c2475fcfa4
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2025-01-19 23:44:04 +0000

    Atualizar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,3 @@
-function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
-    } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
-    }
+function login() {
+    window.location.href = "./html/login/login.html";
 }
