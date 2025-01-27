5f36c895b11b207ad4ccd858cab8b1bd1a31a746
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2025-01-27 20:49:15 +0000

    Atualizar

--- html/play/menu.js
+++ html/play/menu.js
@@ -1,6 +1,5 @@
 const scoreTextPoint = document.getElementById("score");
-sessionStorage.getItem("score_round");
-scoreTextPoint.innerHTML = score_round;
+scoreTextPoint.innerHTML = score_roundsessionStorage.getItem("score_round");
 
 function token() {
   window.location.href = "../quiz/token/token.html";
