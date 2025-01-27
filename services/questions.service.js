e7f544d187490c76482355a342eb521672352f3c
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2025-01-27 22:07:06 +0000

    Atualizar

--- services/questions.service.js
+++ services/questions.service.js
@@ -4,7 +4,7 @@ const questionsService = {
         try {
             const querySnapshot = await firebase.firestore().collection("questions")
             .where('level','==',level)
-            .where('category','===',category)
+            .where('category','==',category)
             .get();
 
             if(querySnapshot.empty){
