09cc33540bb301090183b739b8b6d860e84ccc4b
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2025-01-27 21:04:20 +0000

    Atualizar

--- auth-guard.js
+++ auth-guard.js
@@ -2,6 +2,7 @@ firebase.auth().onAuthStateChanged( (user) => {
     const userUid = user.uid;
     sessionStorage.setItem("userUid", user.uid);
     sessionStorage.setItem("nameUser",user.nickname);
+    sessionStorage.setItem("score_total",0);
     document.getElementById("userUid").value = user.uid;
     userService.findByUid(userUid).then (user=>{
         document.getElementById("nameUser").innerHTML = "Olá, " + user.nickname;
