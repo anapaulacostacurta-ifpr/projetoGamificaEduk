42d16e885b6bf9104a7b769d85a662160c5352ef
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2025-01-27 23:10:36 +0000

    Atualizar

--- html/home/home.js
+++ html/home/home.js
@@ -2,19 +2,6 @@
 const scorePoint = document.getElementById("score_total");
 scorePoint.innerHTML = "Score Total: "+sessionStorage.getItem("score_total");
 
-function adm() {
-    window.location.href = "../adm/conf/conf.html";
-}
-
-
-function newQuestion() {
-    window.location.href = "../adm/questions/questions.html";
-}
-
-function newBoardGame() {
-    window.location.href = "../adm/boardgames/boardgames.html";
-}
-
 function jogar() {
     window.location.href = "../play/play.html";
 }
