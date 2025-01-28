9deaa4f5f4a963814fa34a1e99dc63e4532f6288
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2025-01-28 17:58:06 +0000

    Atualizar

--- html/quiz/quiz.js
+++ html/quiz/quiz.js
@@ -142,7 +142,6 @@ function startTimer(time) {
       for (i = 0; i < allOptions; i++) {
         option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
       }
-      next_btn.classList.add("show"); //show the next button if user selected any option
     }
   }
 }
