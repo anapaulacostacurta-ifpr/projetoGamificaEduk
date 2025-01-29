da4f55d288d1294e5fb128b3b879cfe8a6488b91
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2025-01-29 01:25:08 +0000

    Atualizar

--- services/logboardgames.service.js
+++ services/logboardgames.service.js
@@ -6,7 +6,8 @@ const logboardgamesService = {
             .set(logboardgames);
     },
     getlogboardgameByUserUID: async (user_UID,boardgame_id,level,data) => {
-        const querySnapshot = await firebase.firestore().collection("logboardgames").doc(user_UID)
+        const querySnapshot = await firebase.firestore().collection("logboardgames")
+        .where('user_UID','==', user_UID)
         .where('boardgame_id','==',boardgame_id)
         .where('level','==',level)
         .where('data','==',data)
