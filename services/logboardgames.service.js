f1b6c2284775b1b8a08e8894763e8d420194177b
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2025-01-29 15:39:34 +0000

    Atualizar

--- services/logboardgames.service.js
+++ services/logboardgames.service.js
@@ -11,7 +11,6 @@ const logboardgamesService = {
         const querySnapshot = await firebase.firestore().collection("logboardgames")
         .doc(user_UID)
         .collection("log_answers");
-        
         if(querySnapshot.empty){
             console.log("Log vazia:" + user_UID);
             return [];
@@ -19,14 +18,13 @@ const logboardgamesService = {
         const logboardgames = querySnapshot.docs.map(doc=>doc.data());
         logboardgames.forEach(logboardgame => {
             var log_answers = logboardgame.log_answers;
-            if (log_answers.boardgame_id == boardgame_id){
+              if (log_answers.boardgame_id == boardgame_id){
                 if(log_answers.level == level){
                     if(log_answers.data == data){
                         return log_answers;
                     }
                 }
               }
-            }
         });   
         console.log("Log vazia:" + user_UID +","+boardgame_id+","+level+","+data);
         return [];
