85383e5665eb611e176e3bc65030a95c26557eb4
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2025-01-29 03:34:08 +0000

    Atualizar

--- html/quiz/quiz.js
+++ html/quiz/quiz.js
@@ -117,8 +117,8 @@ document.getElementById("quiz-form").addEventListener("submit", function(event) 
   const boardgame_id = boardgame.boardgameid;
   const level = boardgame.level;
   const data = (new Date()).toLocaleDateString('pt-BR');
-  //var user_answer = getUserAnswers(user_UID,boardgame_id,level,data);
-  var user_answer = new Array(); 
+  var user_answer = getUserAnswers(user_UID,boardgame_id,level,data);
+  //var user_answer = new Array(); 
   const log = {category: question.type, question_numb:question.numb, user_answer:sessionStorage.userAnswer, tokenid: sessionStorage.token};
   user_answer.push(log);
   const log_answers = {
