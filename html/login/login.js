17a34609f489a5b4f5970c7347f3a05fe78835fb
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2025-02-02 00:34:33 +0000

    Atualizar

--- html/login/login.js
+++ html/login/login.js
@@ -1,21 +1,5 @@
 firebase.auth().onAuthStateChanged( (user) => {
     if (user) {
-        sessionStorage.setItem("userUid", user.uid);
-        userService.findByUid(uid).then (user=>{
-            if(user === undefined){
-                sessionStorage.setItem("profile_atualizar",true);
-            }else{
-                sessionStorage.setItem("profile_atualizar",false);
-                document.getElementById("nameUser").innerHTML = user.name;
-                sessionStorage.setItem("score_total",user.score);
-                const profiles = user.profiles;
-                sessionStorage.setItem("admin",profiles.admin);
-                sessionStorage.setItem("professor",profiles.admin);
-                sessionStorage.setItem("aluno",profiles.admin);
-            }
-        }).catch(error => {
-            console.log(getErrorMessage(error));
-        });
         window.location.href = "../home/home.html";
     }
 })
