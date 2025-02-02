4e0054f39e32201e21af24e0d340a6562722895c
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2025-02-02 23:43:14 +0000

    Atualizar

--- html/home/home.js
+++ html/home/home.js
@@ -6,7 +6,6 @@ firebase.auth().onAuthStateChanged( (user) => {
 })
 var user_UID = sessionStorage.userUid;
 if (sessionStorage.nameUser === undefined){
-    getCurrentUser(user_UID);
     var usercurrent = getCurrentUser(user_UID);
     if ( !(usercurrent === undefined)){
         showMenuProfessor();
