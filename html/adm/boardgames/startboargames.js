083fc1d9a333b340fd7df5ce24bfd82d196567cd
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2025-02-02 15:07:30 +0000

    Atualizar

---
+++ html/adm/boardgames/startboargames.js
@@ -0,0 +1,62 @@
+var user_UID = sessionStorage.userUid;
+var score_total = sessionStorage.score_total + " points";
+var nameUser = sessionStorage.nameUser;
+var lista_boardgames = document.getElementById("lista_boardgames");
+lista_boardgames.style.display = "none";  
+
+var alert_sucesso = document.getElementById("alert_sucesso");
+alert_sucesso.style.display = "none";   
+
+firebase.auth().onAuthStateChanged( (user) => {
+  if (!user) {
+    sessionStorage.clear;
+    window.location.href = "../login/login.html";
+  }
+})
+
+// Captura o evento de envio do formulário
+document.getElementById("startboardgame-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  // Captura os dados do formulário
+  const round_date = (new Date()).toLocaleDateString('pt-BR');
+  const level = document.getElementById("level").value;
+  const host = sessionStorage.userUid;
+  const boardgameid = document.getElementById("boardgameid").value;
+  const state = "waiting"; // "waiting", "started", "finished"
+
+  let linhas = ''; 
+  var boardgames = boardgamesService.getBoardGameByID(boardgameid, round_date, host, level, state);
+  boardgames.array.forEach(boardgames => {
+    boardgames.forEach(boardgame => {
+            let boardgame_id = '<td><span>'+boardgame.boardgame_id+'</span></td>';
+            let host = '<td><span>'+boardgame.host+'</span></td>';
+            let level = '<td><span>'+boardgame.level+'</span></td>';
+            let round_data = '<td><span>'+boardgame.round_data+'</span></td>';
+            let status = '<td><span>'+boardgame.status+'</span></td>';
+            linhas = linhas +'<tr>'+boardgame_id+host+level+round_data+status+'</tr>';
+        })
+        let tbody = '<tbody>'+linhas+'</tbody>';
+        let thead = '<thead><tr><th>BoardgameID</th><th>Professor</th><th>Level</th><th>Data</th><th>Status</th></tr></thead>';     
+        let table = '<table class="table table-bordered">'+ thead + tbody+'</table>';
+        lista_boardgames.innerHTML = table;
+    }).catch((error) => {
+        let errorString = '<span>'+ error+'<span>';
+        lista_boardgames.innerHTML = errorString;
+  });  
+});
+
+function logout() {
+  firebase.auth().signOut().then(() => {
+      sessionStorage.clear();
+      window.location.href = "../../home/home.html";
+  }).catch(() => {
+      alert('Erro ao fazer logout');
+  })
+}
+
+function voltar(){
+  window.location.href = "../../home/home.html";
+}
+
+  
--- services/boardgames.service.js
+++ services/boardgames.service.js
@@ -31,12 +31,13 @@ const boardgamesService = {
             console.log(boardgame);
             return boardgame;
     },
-    getBoardGameByID: async (boardgameid,round_date, host, level) => {
+    getBoardGameByID: async (boardgameid,round_date, host, level, state) => {
         const querySnapshot = await firebase.firestore().collection("boardgames")
         .where('boardgameid','==',boardgameid)
         .where('round_date','==',round_date)
         .where('host','==',host)
         .where('level','==',level)
+        .where('state','==',state)
         .get();
         console.log(querySnapshot);
 
