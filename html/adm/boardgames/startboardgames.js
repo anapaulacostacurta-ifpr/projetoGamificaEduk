99c8a2091d39b15a549164f01f3d5457dc227c50
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2025-02-04 20:06:51 +0000

    Atualizar

--- html/adm/boardgames/startboardgames.js
+++ html/adm/boardgames/startboardgames.js
@@ -26,13 +26,12 @@ document.getElementById("startboardgame-form").addEventListener("submit", functi
   boardgamesService.getBoardGameByID(boardgameid, round_date, professor, level, state).then(boardgames => {
     boardgames.forEach(boardgame => {
             var option = boardgame.boardgameid+";"+boardgame.level+";"+boardgame.round_date+";"+professor;
-            let boardgame_id = '<td><span>'+boardgame.boardgameid+'</span></td>';
+            let boardgame_id = '<td><span>'+'<label class="form-check-label" for="'+boardgame.boardgameid+'">'+boardgame.boardgameid+'</span></label></td>';
             let level = '<td><span>'+boardgame.level+'</span></td>';
             let round_data = '<td><span>'+boardgame.round_date+'</span></td>';
             let state = '<td><span>'+boardgame.state+'</span></td>';
             let radio = '<input type="radio" class="form-check-activate" id="radio_id" name="radio_id" value="'+option+'" checked">';
-            let label = '<label class="form-check-label" for="'+boardgame.boardgameid+'">'+'<tr>'+boardgame_id+level+round_data+state+'</tr>'+'</label>';
-            linhas = linhas + radio + label;
+            linhas = linhas + '<tr>'+radio+boardgame_id+level+round_data+state'</tr>';
         })
         let tbody = '<tbody>'+linhas+'</tbody>';
         let thead = '<thead><tr><th></th><th>BoardgameID</th><th>Level</th><th>Data</th><th>Status</th></tr></thead>';     
