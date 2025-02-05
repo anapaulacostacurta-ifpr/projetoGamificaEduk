4a7dbd4fca6b1fd6cc901ad9db06a0cb45bc6a7a
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2025-02-05 18:52:53 +0000

    Atualizar

--- html/play/play.js
+++ html/play/play.js
@@ -98,6 +98,8 @@ function getProfile(){
   if(User === undefined){
       User = getUser();
   }
-  document.getElementById("nameUser").innerHTML = User.name;
-  document.getElementById("score_total").innerHTML = User.score +" points";
+  document.getElementById("nameUser").innerHTML = User.nickname;
+  var avatar = User.avatar;
+  document.getElementById("avatarUser").innerHTML ='<img class="img-fluid rounded-circle img-thumbnail" src="../../assets/img/perfil/'+avatar+'.png" width="50" height="50"></img>';
+  document.getElementById("score_total").innerHTML = User.score;
 }
