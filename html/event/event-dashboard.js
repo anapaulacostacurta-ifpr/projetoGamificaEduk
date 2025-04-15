d7c5df517d3bfc25eee52c8ba07024c8a0fb2d2c
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2025-04-15 01:27:41 +0000

    Atualizar

--- html/event/event-dashboard.js
+++ html/event/event-dashboard.js
@@ -6,35 +6,37 @@ firebase.auth().onAuthStateChanged((User) => {
       if (!(enroll_events.length === 0)){
         let card_active_event = ``;
         let card_closed_event = ``;
-        eventService.getEventByUID(enroll_events.event_id).then(event =>{
-          let card_event = `<span class="event_dados" id="${event.uid}">${event.dados.name} - ${event.dados.date}</span>`;
-          card_coins = 
-                `<span id="coin" class="col-sm-3 ml-auto">`+
-                  `<span class="badge rounded-pill bg-success">`+
-                      `<span id="coins" class="badge bg-light text-dark">${event.coins}</span>`+
-                  `&nbsp;AB@ COINS`+
-                  `</span>`+
-                  `<br/>`+
-                `</span>`;
-          if (event.dados.state === "started"){
-            card_active_event = card_active_event +`<div class="card card_active">${card_event}${card_coins}</div>`;
-          }
-          if (event.dados.state === "finished"){
-            card_closed_event = card_closed_event +`<div class="card card_closed">${card_event}${card_coins}</div>`;
-          }
-          active_events_list.innerHTML = card_active_event;
-          closed_events_list.innerHTML = card_closed_event;
-          const card_active = active_events_list.querySelectorAll(".card_active");
-          const card_closed = closed_events_list.querySelectorAll(".card_closed");
-          // set onclick attribute to all available cards active
-          for (i = 0; i < card_active.length; i++) {
-            card_active[i].setAttribute("onclick", "cardActiveSelected(this)");
-          }
-          // set onclick attribute to all available cards closed
-          for (i = 0; i < card_closed.length; i++) {
-            card_closed[i].setAttribute("onclick", "cardClosedSelected(this)");
-          }
-        });   
+        enroll_events.forEach(enroll_event => {
+          eventService.getEventByUID(enroll_event.event_id).then(event =>{
+            let card_event = `<span class="event_dados" id="${event.uid}">${event.dados.name} - ${event.dados.date}</span>`;
+            card_coins = 
+                  `<span id="coin" class="col-sm-3 ml-auto">`+
+                    `<span class="badge rounded-pill bg-success">`+
+                        `<span id="coins" class="badge bg-light text-dark">${event.coins}</span>`+
+                    `&nbsp;AB@ COINS`+
+                    `</span>`+
+                    `<br/>`+
+                  `</span>`;
+            if (event.dados.state === "started"){
+              card_active_event = card_active_event +`<div class="card card_active">${card_event}${card_coins}</div>`;
+            }
+            if (event.dados.state === "finished"){
+              card_closed_event = card_closed_event +`<div class="card card_closed">${card_event}${card_coins}</div>`;
+            }
+            active_events_list.innerHTML = card_active_event;
+            closed_events_list.innerHTML = card_closed_event;
+            const card_active = active_events_list.querySelectorAll(".card_active");
+            const card_closed = closed_events_list.querySelectorAll(".card_closed");
+            // set onclick attribute to all available cards active
+            for (i = 0; i < card_active.length; i++) {
+              card_active[i].setAttribute("onclick", "cardActiveSelected(this)");
+            }
+            // set onclick attribute to all available cards closed
+            for (i = 0; i < card_closed.length; i++) {
+              card_closed[i].setAttribute("onclick", "cardClosedSelected(this)");
+            }
+          });   
+        })
       }
     });
   }
