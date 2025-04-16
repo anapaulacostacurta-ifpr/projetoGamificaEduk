c7466b9fa4d1480754d03b49069df21f47c9b825
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2025-04-16 19:26:35 +0000

    Atualizar

--- services/activitytasks.service.js
+++ services/activitytasks.service.js
@@ -1,15 +1,22 @@
 const activityTaskService = {
     getTaskActivity: async (activity_id) => {
-        try{
-            return querySnapshot = await firebase.firestore().collection("activity_tasks")
+        const querySnapshot = await firebase.firestore().collection("activity_tasks")
                 .where('activity_id','==',activity_id)
-                .get()
-                .then(doc => {
-                    return doc.data();
+                .get();
+                console.log(querySnapshot);
+    
+                if(querySnapshot.empty){
+                    return [];
+                }
+                var activity_tasks = new Array();
+                querySnapshot.forEach(doc => {
+                    var uid = doc.id;
+                    var dados = doc.data();
+                    var activity_task = {uid,dados};
+                    activity_tasks.push(activity_task);
                 });
-        }catch (error) {
-            throw error;
-        }
+                console.log(activity_tasks);
+                return activity_tasks;
     },
     save: async (activity_task) => {
         try{
