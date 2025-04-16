4f4bd0547ad99593280e75063518134eee011574
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2025-04-16 19:55:15 +0000

    Atualizar

--- html/play/menu.js
+++ html/play/menu.js
@@ -7,36 +7,38 @@ firebase.auth().onAuthStateChanged((User) => {
       activity_uid = params.get('activity_uid');
       activityService.getActivitybyUid(activity_uid).then((activity) => {
         let menu = ``;
-        activityTaskService.getTaskActivity(activity_uid).then(activity_task => {
-          if(!(activity_task.length == 0)){ 
-            if (!(activity_task.quizzes_id==="")){
-              menu = menu +`<p><button type="button" class="badge bg-primary p-2" id="btnQuiz" onclick="btnQuiz()">QUIZ</button></p>`;
-            }
-            if(!(activity_task.challange_id==="")){
-              menu = menu + `<p><button type="button" class="badge bg-primary p-2" id="btnDesafio" onclick="btnDesafio()">DESAFIO</button></p>`;
-            }
-            if(!(activity_task.orienteering_id==="")) {
-              menu = menu + `<p><button type="button" class="badge bg-primary p-2" id="btnOrientacao" onclick="btnOrientacao()">ORIENTAÇÃO</button></p>`;
-            }
-            if( (!(activity_task.good_fortune_id==="")) && (!(activity_task.tough_luck_id==="")) ){
-              menu = menu + `<p><button type="button" class="badge bg-primary p-2" id="btnSorte" onclick="btnSorteouReves()">SORTE OU REVÉS</button></p>`;
-            }
-            if (!(activity_task.bonus_id==="")){
-              menu = menu + `<p><button type="button" class="badge bg-warning p-2" id="btnTarefas" onclick="btnBonus()">TAREFAS</button></p>`; 
-            }       
-            if(!(activity_task.quiz_final_id==="")){
-              menu = menu + `<p><button type="button" class="badge bg-success p-2 border border-2 border-dark" id="btnQuizfinal" onclick="btnQuizfinal()">QUIZ FINAL</button></p>`;
-            }
-            main_menu.innerHTML = menu;
-          }else{
-            main_menu.innerHTML = "Nenhuma atividade cadatrada. Fale com o Administrador do Evento!";
-          }
-          document.getElementById("level").innerHTML = activity.level;
-          checkinactivityService.getcheckinbyPlayer(activity_uid,User.uid).then(checkin_ativities =>{
-            checkin_ativities.forEach(checkin_ativity => {
-              document.getElementById("points").innerHTML = checkin_ativity.dados.points;
+        activityTaskService.getTaskActivity(activity_uid).then(activity_tasks => {
+          if(activity_tasks.length == 0){
+            activity_tasks.forEach(activity_task => {           
+                if (!(activity_task.dados.quizzes_id==="")){
+                  menu = menu +`<p><button type="button" class="badge bg-primary p-2" id="btnQuiz" onclick="btnQuiz()">QUIZ</button></p>`;
+                }
+                if(!(activity_task.dados.challange_id==="")){
+                  menu = menu + `<p><button type="button" class="badge bg-primary p-2" id="btnDesafio" onclick="btnDesafio()">DESAFIO</button></p>`;
+                }
+                if(!(activity_task.dados.orienteering_id==="")) {
+                  menu = menu + `<p><button type="button" class="badge bg-primary p-2" id="btnOrientacao" onclick="btnOrientacao()">ORIENTAÇÃO</button></p>`;
+                }
+                if( (!(activity_task.dados.good_fortune_id==="")) && (!(activity_task.dados.tough_luck_id==="")) ){
+                  menu = menu + `<p><button type="button" class="badge bg-primary p-2" id="btnSorte" onclick="btnSorteouReves()">SORTE OU REVÉS</button></p>`;
+                }
+                if (!(activity_task.dados.bonus_id==="")){
+                  menu = menu + `<p><button type="button" class="badge bg-warning p-2" id="btnTarefas" onclick="btnBonus()">TAREFAS</button></p>`; 
+                }       
+                if(!(activity_task.dados.quiz_final_id==="")){
+                  menu = menu + `<p><button type="button" class="badge bg-success p-2 border border-2 border-dark" id="btnQuizfinal" onclick="btnQuizfinal()">QUIZ FINAL</button></p>`;
+                }
+                main_menu.innerHTML = menu;         
+                document.getElementById("level").innerHTML = activity.level;
+                checkinactivityService.getcheckinbyPlayer(activity_uid,User.uid).then(checkin_ativities =>{
+                  checkin_ativities.forEach(checkin_ativity => {
+                    document.getElementById("points").innerHTML = checkin_ativity.dados.points;
+                  })
+                })
             })
-          })
+          }else{
+            menu = "Nenhuma atividade cadastrada. Entre em contato com Adminsitrador do Evento!";
+          }
         })
       })
     })
