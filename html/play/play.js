4c48ca0b32212a3ac59c996a2c32f55c4444fedb
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2025-04-19 02:14:13 +0000

    Atualizar

--- html/play/play.js
+++ html/play/play.js
@@ -8,20 +8,23 @@ firebase.auth().onAuthStateChanged((User) => {
       event.preventDefault();
       const activity_id = document.getElementById("activity_id").value;
       try {
-       verificarAtividade(activity_id).then(noPrazo =>{
-        if (noPrazo) {
-          verificarCheckin(activity_id, user_UID).then(checkin_ok=>{
-            if (checkin_ok) {
-              alert('Retornando para atividade!');
-            } else {
-              alert('Realizado check-in na atividade!');
-              doCheckin(activity_id, user_UID); 
-            }
-          })
-        } else {
+       
+        activityService.getActivities(activity_id).then (activities => {
+          var activity = validarAtivities(activities);
+          if (activity !== null){
+            checkinactivityService.getcheckinbyPlayer(activity_id, user_UID).then(checkin_activities =>{
+              var checkin_player = (checkin_activities.length === 1) ? checkin_activities[0] : null;
+              if (checkin_player != null) {
+                alert('Retornando para atividade!');
+              } else {
+                alert('Realizado check-in na atividade!');
+                doCheckin(activity_id, user_UID); 
+              }
+            })
+          } else {
             console.log("Atividade fora do prazo!");
           }
-       })
+        })
       }catch (error) {
         alert("Erro: " + error.message);
         console.error(error);
@@ -30,59 +33,33 @@ firebase.auth().onAuthStateChanged((User) => {
       //window.location.href = "./menu.html?activity_id="+activity_id;
       });
 
-      async function verificarCheckin(activity_id, user_UID) {
-        await getcheckinbyPlayer(activity_id, user_UID).then(checkin_player =>{
-          if (checkin_player != null) {
-            // points = checkin_player.points; // se precisar usar
-            checkin_ok = true;
-          } else {
-            checkin_ok = false;
-          }
-          return checkin_ok;
-        });
-        
-      }
-
-      async function getcheckinbyPlayer(activity_id, user_UID) {
-        await checkinactivityService.getcheckinbyPlayer(activity_id, user_UID).then(checkin_activities =>{
-          return (checkin_activities.length === 1) ? checkin_activities[0] : null;
-        })
-      }
-  
-      async function verificarAtividade(activity_id) {
-        await getActivitiesByID(activity_id).then(activity => {
-          var noPrazo = (activity !== null)
-          return noPrazo; // true se dentro do prazo
-        })
-      }
-
-      async function getActivitiesByID(activity_id) {
-        await activityService.getActivities(activity_id).then (activities => {
-          const date = new Date();
+   
+      function validarAtivities(activities) {
           if (activities.length > 1) {
             alert("Verificar com o administrador do Evento a configuração da atividade");
             return null;
-          }
-          if (activities.length === 1) {
-            const activity = activities[0].dados;
-            if (activity.id === activity_id) {
-              const [dStart, mStart, yStart] = activity.date_start.split("/");
-              const [hStart, minStart] = activity.time_start.split(":");
-              const data_time_start = new Date(yStart, mStart - 1, dStart, hStart, minStart);
-    
-              const [dEnd, mEnd, yEnd] = activity.date_final.split("/");
-              const [hEnd, minEnd] = activity.time_final.split(":");
-              const data_time_final = new Date(yEnd, mEnd - 1, dEnd, hEnd, minEnd);
-    
-              if (date >= data_time_start && date <= data_time_final) {
-                return activities[0];
-              } else {
-                alert("Atividade fora do prazo!");
+          }else{
+            if (activities.length === 1) {
+              const activity = activities[0].dados;
+              if (activity.id === activity_id) {
+                const [dStart, mStart, yStart] = activity.date_start.split("/");
+                const [hStart, minStart] = activity.time_start.split(":");
+                const data_time_start = new Date(yStart, mStart - 1, dStart, hStart, minStart);
+      
+                const [dEnd, mEnd, yEnd] = activity.date_final.split("/");
+                const [hEnd, minEnd] = activity.time_final.split(":");
+                const data_time_final = new Date(yEnd, mEnd - 1, dEnd, hEnd, minEnd);
+      
+                if (date >= data_time_start && date <= data_time_final) {
+                  return activities[0];
+                } else {
+                  alert("Atividade fora do prazo!");
+                }
               }
+            }else{
+              return null;
             }
           }
-          return null;
-        })
       }
 
       function doCheckin(activity_id, user_UID) {
