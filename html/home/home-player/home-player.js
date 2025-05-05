a8bd8a4b9ca477fa841bcc385b9e477ae3723321
ANA PAULA COSTACURTA <anapaula.costacurta@ifpr.edu.br>
2025-05-05 01:08:00 +0000

    Atualizar

--- html/home/home-player/home-player.js
+++ html/home/home-player/home-player.js
@@ -23,7 +23,7 @@ firebase.auth().onAuthStateChanged((User) => {
 
 function logout() {
     firebase.auth().signOut().then(() => {
-        window.location.href = "../login/login.html";
+        window.location.href = "../../login/login.html";
     }).catch(() => {
         alert('Erro ao fazer logout');
     })
