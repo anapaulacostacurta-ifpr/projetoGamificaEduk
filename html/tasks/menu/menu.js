78d5aca8c77b7893c8fffdf2a4e865506193fa4a
ANA PAULA COSTACURTA <anapaula.costacurta@ifpr.edu.br>
2025-05-06 11:21:11 +0000

    Atualizar

--- html/tasks/menu/menu.js
+++ html/tasks/menu/menu.js
@@ -5,51 +5,57 @@ firebase.auth().onAuthStateChanged((User) => {
     const params = new URLSearchParams(window.location.search);
     activity_id = params.get('activity_id');
     activityService.getActivitybyUid(activity_id).then((activity) => {
-      let menu = ``;
-      activityTaskService.getTaskActivity(activity_id).then(activity_tasks => {
-        if(!(activity_tasks.length == 0)){
-          activity_tasks.forEach(activity_task => {
-            activityContentsService.getContentsActivity(activity_id).then(activity_contents =>{
-              activity_contents.forEach(activity_content =>{
-                if (!(activity_content.dados.all_viewed)){
-                  menu = menu +`<p><button type="button" class="badge bg-primary p-2" id="btnConteudo" onclick="btnConteudo()">CONTEÚDO</button></p>`;
-                }
-                if (!(activity_task.dados.quizzes_id==="")){
-                  menu = menu +`<p><button type="button" class="badge bg-primary p-2 btnQuiz" id="${activity_task.dados.quizzes_id}" onclick="btnQuiz()">QUIZ</button></p>`;
-                }
-                if(!(activity_task.dados.challenge_id==="")){
-                  menu = menu + `<p><button type="button" class="badge bg-primary p-2 btnDesafio" id="${challenge_id}" onclick="btnDesafio()">DESAFIO</button></p>`;
-                }
-                if(!(activity_task.dados.orienteering_groups_id==="")) {
-                  menu = menu + `<p><button type="button" class="badge bg-primary p-2 btnOrientacao" id="${activity_task.dados.orienteering_groups_id}" onclick="btnOrientacao()">ORIENTAÇÃO</button></p>`;
-                }
-                if( (!(activity_task.dados.good_fortune_id==="")) && (!(activity_task.dados.tough_luck_id==="")) ){
-                  menu = menu + `<p><button type="button" class="badge bg-primary p-2 btnSorteouReves" id="${activity_task.dados.good_fortune_id};${activity_task.dados.tough_luck_id}" onclick="btnSorteouReves()">SORTE OU REVÉS</button></p>`;
-                }
-                if (!(activity_task.dados.bonus_id==="")){
-                  menu = menu + `<p><button type="button" class="badge bg-warning p-2 btnBonus" id="${activity_task.dados.bonus_id}" onclick="btnBonus()">TAREFAS</button></p>`; 
-                }       
-                if(!(activity_task.dados.quiz_final_id==="")){
-                  menu = menu + `<p><button type="button" class="badge bg-success p-2 border border-2 border-dark btnQuizfinal" id="${activity_task.dados.quiz_final_id}" onclick="btnQuizfinal()">QUIZ FINAL</button></p>`;
-                }
-                main_menu.innerHTML = menu;         
-                document.getElementById("level").innerHTML = activity.level;
-                checkinactivityService.getcheckinbyPlayer(activity_id,User.uid).then(checkin_ativities =>{
-                  checkin_ativities.forEach(checkin_ativity => {
-                    document.getElementById("points").innerHTML = checkin_ativity.dados.points;
-                  })
+      if(validarValor(activity)){
+        let menu = ``;
+        activityTaskService.getTaskActivity(activity_id).then(activity_tasks => {
+          if(!(activity_tasks.length == 0)){
+            activity_tasks.forEach(activity_task => {
+              if (!(activity_task.dados.activity_contents_id === "")){
+                menu = menu +`<p><button type="button" class="badge bg-primary p-2" id="btnConteudo" onclick="btnConteudo()">CONTEÚDO</button></p>`;
+              }
+              if (!(activity_task.dados.quizzes_id==="")){
+                menu = menu +`<p><button type="button" class="badge bg-primary p-2 btnQuiz" id="${activity_task.dados.quizzes_id}" onclick="btnQuiz()">QUIZ</button></p>`;
+              }
+              if(!(activity_task.dados.challenge_id==="")){
+                menu = menu + `<p><button type="button" class="badge bg-primary p-2 btnDesafio" id="${challenge_id}" onclick="btnDesafio()">DESAFIO</button></p>`;
+              }
+              if(!(activity_task.dados.orienteering_groups_id ==="")) {
+                menu = menu + `<p><button type="button" class="badge bg-primary p-2 btnOrientacao" id="${activity_task.dados.orienteering_groups_id}" onclick="btnOrientacao()">ORIENTAÇÃO</button></p>`;
+              }
+              if((!(activity_task.dados.good_fortune_id==="")) && (!(activity_task.dados.tough_luck_id===""))){
+                menu = menu + `<p><button type="button" class="badge bg-primary p-2 btnSorteouReves" id="${activity_task.dados.good_fortune_id};${activity_task.dados.tough_luck_id}" onclick="btnSorteouReves()">SORTE OU REVÉS</button></p>`;
+              }
+              if (!(activity_task.dados.bonus_id==="")){
+                menu = menu + `<p><button type="button" class="badge bg-warning p-2 btnBonus" id="${activity_task.dados.bonus_id}" onclick="btnBonus()">TAREFAS</button></p>`; 
+              }       
+              if(!(activity_task.dados.quiz_final_id==="")){
+                menu = menu + `<p><button type="button" class="badge bg-success p-2 border border-2 border-dark btnQuizfinal" id="${activity_task.dados.quiz_final_id}" onclick="btnQuizfinal()">QUIZ FINAL</button></p>`;
+              }
+              main_menu.innerHTML = menu;         
+              document.getElementById("level").innerHTML = activity.level;
+              checkinactivityService.getcheckinbyPlayer(activity_id,User.uid).then(checkin_ativities =>{
+                checkin_ativities.forEach(checkin_ativity => {
+                  document.getElementById("points").innerHTML = checkin_ativity.dados.points;
                 })
               })
             })
-          })
-        }else{
-          menu = "Nenhuma atividade cadastrada. Entre em contato com Adminsitrador do Evento!";
-        }
-      })
+          }else{
+            menu = "Nenhuma atividade cadastrada. Entre em contato com Adminsitrador do Evento!";
+          }
+        })
+      }else{
+        alert("Não encontrou a atividade. Entre em contato com Adminsitrador do Evento!");
+      }
     })
-
   }
 });
+
+function validarValor(valor) {
+  if (valor === null) {
+    return false;
+  }
+  return true;
+}
 
 function btnQuiz() {
   let botao = document.querySelector('.btnQuiz');
