95d8906bdcc6ea6c9bffea38b84d72143ea4fdba
ANA PAULA COSTACURTA <anapaula.costacurta@ifpr.edu.br>
2025-05-19 00:33:05 +0000

    FB-007.1 - Criar layout HTML da Home do Jogador: Refatoramento

--- html/home/home.js
+++ html/home/home.js
@@ -1,28 +1,43 @@
-firebase.auth().onAuthStateChanged((User) => {
-    if (User) {
-        userService.findByUid(User.uid).then(user=>{
-            if(user.profile === "player"){
-                window.location.href = "./home-player/home-player.html";
-            }
-            if(user.profile === "host"){
-                window.location.href = "./home-host/home-host.html";
-            }
-        }).catch(error => {
-            if(error.message === "01 - Não encontrado."){
-                alert("Seu perfil precisa ser atualizado e ativado!Acesse o menu perfil.");
-                window.location.href = "../profile/update-profile.html";
-            }
-            console.log(error);
-        });
+// Escuta mudanças na autenticação
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        // Busca dados do usuário autenticado
+        userService.findByUid(user.uid)
+            .then(userData => {
+                switch (userData.profile) {
+                    case "player":
+                        window.location.href = "./home-player/home-player.html";
+                        break;
+                    case "host":
+                        window.location.href = "./home-host/home-host.html";
+                        break;
+                    default:
+                        alert("Perfil desconhecido.");
+                        break;
+                }
+            })
+            .catch(error => {
+                // Se o erro for perfil não encontrado
+                if (error.message === "01 - Não encontrado.") {
+                    alert("Seu perfil precisa ser atualizado e ativado! Acesse o menu perfil.");
+                    window.location.href = "../profile/update-profile.html";
+                } else {
+                    console.error("Erro ao buscar perfil:", error);
+                }
+            });
     }
-})
+});
 
+// Realiza logout do Firebase
 function logout() {
-    firebase.auth().signOut().then(() => {
-        window.location.href = "../login/login.html";
-    }).catch(() => {
-        alert('Erro ao fazer logout!');
-    })
+    firebase.auth().signOut()
+        .then(() => {
+            window.location.href = "../login/login.html";
+        })
+        .catch(() => {
+            alert("Erro ao fazer logout!");
+        });
 }
+
 
 
