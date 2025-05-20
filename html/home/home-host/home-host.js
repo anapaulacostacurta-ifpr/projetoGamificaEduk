eed79fe3df1119e6d53253a1ebb3d6f77bf64fe9
ANA PAULA COSTACURTA <anapaula.costacurta@ifpr.edu.br>
2025-05-20 01:18:36 +0000

    NAV-001.2 - Proteger rota via onAuthStateChanged() para validar sessão

--- html/home/home-host/home-host.js
+++ html/home/home-host/home-host.js
@@ -19,12 +19,5 @@ firebase.auth().onAuthStateChanged((User) => {
     }
 })
 
-function logout() {
-    firebase.auth().signOut().then(() => {
-        window.location.href = "../../login/login.html";
-    }).catch(() => {
-        alert('Erro ao fazer logout');
-    })
-}
 
 
